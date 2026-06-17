9679ff80c08b636ec39f6a8e74bb9549abc47291
Mohd Suleman <mohdsulemanmdk1@gmail.com>
2026-06-17 15:59:08 +0530

    JsDay 1-5

---
+++ DAY 5/assignment.js
@@ -0,0 +1,90 @@
+// -----------------------------------------------------------------------------
+// prolem 1 :  Write a for loop that prints all numbers from 1 to 10.
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+
+
+
+
+// -----------------------------------------------------------------------------
+// prolem 2 :Write a for loop that prints only even numbers from 2 to 20. 
+
+
+
+
+
+// for(let i = 2; i<=20;i+=2){
+//     console.log(i)
+// }
+
+
+
+// -----------------------------------------------------------------------------
+// prolem 3 : Write a for loop that counts down from 10 to 1, then prints "Blast off!".
+
+
+
+
+//  for (let i=10;i>=1;i--) {
+//      console.log(i)
+//     }
+//     console.log("Blast off")
+
+
+
+
+
+// -----------------------------------------------------------------------------
+// prolem 4 :Write a for loop that prints the multiplication table of 7 (from 7 × 1 to 7 × 10).
+
+
+// for(i=1;i<=10;i++){
+//     console.log(`7 x ${i} = ${7*i}`)
+// }
+
+
+
+
+
+// -----------------------------------------------------------------------------
+// prolem 5 :Write a for loop that calculates the sum of numbers from 1 to 20. Store the result in a variable called
+// sum and print it.
+
+
+
+
+// let sum = 0;
+
+// for(let i = 1;i<=20;i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+
+
+ // -----------------------------------------------------------------------------
+// prolem 5 :Use nested for loops to create a 4×4 grid of numbers like this: 
+
+
+
+
+// for(i=1; i<=4 ; i++){
+//     console.log(i )
+//     // for(j=1;j<=4 ;j++){
+//     // console.log(j)
+//     // }
+// }
+
+
+
+ let players =  ["playerA","playerB","playerC","playerD"];
+ for(let player of players){
+    console.log(player);
+ }
+
+ 
