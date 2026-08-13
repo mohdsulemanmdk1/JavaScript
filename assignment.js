bda81cdeca7abe684e936ceabdf7998e4ad2e259
Mohd Suleman <mohdsulemanmdk1@gmail.com>
2026-08-13 07:45:45 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,15 @@
+// ------------------------------------------------------------------------------
+// problem 1 : Create a function called printPattern that prints the following pattern:
+// *
+// **
+// ***
+// ****
+// let j = "";
+// for(let i = 1 ; i<=4;i++){
+//     j+="*"
+//     console.log(j);
+// }
+ let numbers = [10, 20, 30, 40];
+ numbers.forEach((num)=>{
+    console.log(num + 2);
+ })
