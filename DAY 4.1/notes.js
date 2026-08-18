1a16f58bf4d52536b973b97fcb4fe615f6f6eea9
Mohd Suleman <mohdsulemanmdk1@gmail.com>
2026-08-19 00:44:34 +0530

    added notes till 4.1

---
+++ DAY 4.1/notes.js
@@ -0,0 +1,164 @@
+// let array = ['one','two','three','four','five','six']
+// console.log(array)
+// let sliced = array.slice(1,3)
+// console.log(sliced)
+
+
+
+
+
+
+
+// let array = ['one','two','three','four','five','six']
+// console.log(array) 
+// console.log(array.splice(2,2))
+// console.log(array.splice(2,2))
+// console.log(array.splice(-2,2))
+
+
+
+
+
+// let array = ['one','two','three','four','five','six']
+// console.log(array)
+
+// console.log(array.sort())
+// let nums = [9,25,1000,0,82.10]
+// console.log(nums)
+// console.log(nums.sort())
+// console.log(nums.sort((a,b)=>a-b))
+// console.log(nums.sort((a,b)=>b-a))
+
+
+
+
+ 
+
+
+// let array = ['one','two','three','four','five','six']
+// console.log(array)
+// console.log(array.join())
+// console.log(array.join("_"))
+
+
+
+
+
+// let array = [9,25,1000,0,82.10]
+// console.log(array)
+// console.log(array.toString())
+
+
+
+
+// let arr = new Array(3)
+// console.log(arr.length)
+// arr[0] = 1
+// arr[1] = 2
+// arr[2] = 3
+// arr[3] = 4
+// arr[4] = 5
+// arr[5] = 6
+// console.log(arr)
+
+
+
+
+
+// let arr = new Array(1,2,3,4)
+// console.log(arr)
+
+
+
+// let arr = new Array(1,2,3,4)
+// console.log(Array.isArray(arr))
+
+
+
+
+
+// let students = [
+// ["Alice", 20, [85, 90, 92]],
+// ["Bob", 21, [78, 85, 88]],
+// ["Charlie", 19, [92, 88, 95]]
+// ];
+
+
+
+// console.log(students[0][2][1])
+
+
+
+
+
+
+// let arr = new Array(1,2,3,4)
+// arr.length = 11
+// if (Array.isArray(arr) && arr.length>10){
+//     console.log("array's first element is " ,arr[1])
+// }
+// else{
+//     console.log("not an array")
+// }
+
+
+// let arr = new Array(1,2,3,4)
+
+// arr.push(2)
+// console.log(arr)
+
+
+
+// let arr1 = new Array(1,2,3,4)
+// let arr2 = arr1; 
+
+// arr2[0] = 12
+// console.log(arr1)
+// console.log(arr2)
+
+
+
+
+
+
+
+// let num1 = 4
+// let num2 = num1
+// num2 = 3
+// console.log(num1)
+// console.log(num2)
+
+
+
+
+
+
+// let arr = new Array(1,2,3,4)
+// let x = arr[0]
+// arr[0] = arr[2]
+// arr[2] = x
+// console.log(arr)
+
+
+
+
+
+// let items = ["apple", "banana", "mango", "banana"];
+// let index = items.indexOf("banana");
+// items.splice(index,1)
+// console.log(items);
+
+
+
+// let part1 = [1, 2];
+// let part2 = [3, 4];
+// let part3 = [5, 6];
+// let complete = part1.concat(part2).concat(part3);
+// console.log(complete);
+
+
+
+
+
+
+
---
+++ DAY 4/notes.js
@@ -0,0 +1,140 @@
+// console.log("greeting")
+
+
+
+// alert("greeting")
+// let greet = "greeting"
+// let greet2 = greet.toUpperCase()
+// console.log(greet2)
+
+
+
+
+
+
+// let greet = "                greeting             "
+// let greet2 = greet.trim()
+// console.log(greet2)
+
+
+
+
+
+
+
+// let greet = "greeting"
+// let greet2 = greet.slice(0,5)
+// console.log(greet2)
+
+
+
+
+
+
+
+
+// let greet = "greeting"
+// let greet2 = greet.trim().slice(0,6).toUpperCase()
+// console.log(greet2)
+
+
+
+
+// let greet = "greeting"
+// let greet2 =  "suleman"
+// let greet3 = greet +" "+ greet2
+// console.log(greet3)
+
+
+
+
+
+
+
+
+
+
+// let str = "Hello World";
+// console.log(str.indexOf("W"));  
+// console.log(str.indexOf("o"));  
+
+
+// let str = "i love javascript"
+// console.log(str.slice(-1))
+// console.log(str.replace( "love","like"))
+
+
+
+
+
+// let array = ['one', 'two','three','four']
+// console.log(array[array.length -1])
+
+
+
+
+
+
+
+
+// let array = ['one','two','three']
+// console.log(array[array.length -1])
+// array[2] = "zero"
+// // console.log(array)
+// console.log(array[array.length -1])
+
+
+
+// let array = ['one','two','three']
+// console.log(array)
+// array.pop()
+// array.shift()
+// array.unshift("zero")
+// array.push("fourth")
+// console.log(array)
+
+
+
+
+
+
+
+
+
+// let array = ['one','two','three']
+// let array2 = ['one','two','three']
+// console.log(array)
+// console.log(array.indexOf("one"))
+// console.log(array.includes("one"))
+// console.log(array.reverse("one"))
+// console.log(array.concat(array2))
+
+
+// let array = ['one', 'two', 'three']
+// console.log(array)
+// array.splice(0,0,'zero',)
+// console.log(array)
+
+
+
+
+
+// let array = ['one', 'two', 'three']
+// console.log(array)
+// let array2 = array
+// array2.pop()
+// console.log(array)
+// console.log(array2)
+
+
+
+
+// let array = [
+//     [1,2,3],
+//     [4,5,6],
+//     [7,8,9]
+// ]
+
+
+// console.log(array)
+// array[2][1] = 5
